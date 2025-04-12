f9cb0d6a508988c6e7f3550a921fd75868bdb8f1
Bùi Đoàn Hải Yến <buidoanhaiyen@gmail.com>
2025-04-12 15:34:35 +0700

    Cập nhật lại danh sách sản phẩm và đường dẫn hình ảnh

--- data/products.js
+++ data/products.js
@@ -1,20 +1,19 @@
 var list_products = [
     {
-        "masp": "book1",
-        "name": "Đắc Nhân Tâm",
-        "company": "NXB Tổng hợp TPHCM",
-        "img": "img/products/Sach/dac-nhan-tam.jpg", 
-        "price": "88000",
-        "amount": 100,
-        "star": 4,
-        "rateCount": 100,
+        "masp": "Stt1",
+        "name": "Chiến Thắng Trò Chơi Cuộc Sống",
+        "company": "NXB Kim Đồng",
+        "img": "img/products/Sach/Chienthangtrochoicuocsong.jpg",
+        "price": "49000",
+        "star": 3,
+        "rateCount": 26,
         "promo": {
-            "name": "",
-            "value": ""
+            "name": "moiramat",
+            "value": "0"
         },
         "info": {
             "tacgia": "Dale Carnegie",
-            "nxb": "NXB Tổng hợp TPHCM",
+            "nxb": "NXB Kim Đồng",
             "namxb": "2021",
             "sotrang": "320",
             "kichthuoc": "14.5 x 20.5 cm",
@@ -23,17 +22,38 @@ var list_products = [
         }
     },
     {
-        "masp": "book2", 
+        "masp": "Stt2",
+        "name": "Tiếng Kèn Thiên Nga",
+        "company": "NXB Trẻ",
+        "img": "img/products/Sach/Tiengkenthiennga.jpg",
+        "price": "69000",
+        "star": 4,
+        "rateCount": 42,
+        "promo": {
+            "name": "moiramat",
+            "value": "0"
+        },
+        "info": {
+            "tacgia": "E.B. White",
+            "nxb": "NXB Trẻ",
+            "namxb": "2020",
+            "sotrang": "228",
+            "kichthuoc": "13 x 20.5 cm",
+            "loaibia": "Bìa mềm",
+            "theloai": "Văn học thiếu nhi"
+        }
+    },
+    {
+        "masp": "Stt3",
         "name": "Nhà Giả Kim",
         "company": "NXB Văn Học",
-        "img": "img/products/Sach/nha-gia-kim.jpg",
+        "img": "img/products/Sach/Nhagiakim.jpg",
         "price": "79000",
-        "amount": 50,
         "star": 5,
-        "rateCount": 150,
+        "rateCount": 188,
         "promo": {
-            "name": "",
-            "value": ""
+            "name": "giamgia",
+            "value": "5000"
         },
         "info": {
             "tacgia": "Paulo Coelho",
@@ -46,72 +66,135 @@ var list_products = [
         }
     },
     {
-        "masp": "book3",
-        "name": "Tuổi Trẻ Đáng Giá Bao Nhiêu",
-        "company": "NXB Hội Nhà Văn",
-        "img": "img/products/Sach/tuoi-tre-dang-gia-bao-nhieu.jpg",
-        "price": "90000",
-        "amount": 80,
+        "masp": "Stt4",
+        "name": "Đơn Giản",
+        "company": "NXB Thanh Niên",
+        "img": "img/products/Sach/Dongian.jpg",
+        "price": "47000",
+        "star": 5,
+        "rateCount": 7,
+        "promo": {
+            "name": "giamgia",
+            "value": "15000"
+        },
+        "info": {
+            "tacgia": "Nhiều tác giả",
+            "nxb": "NXB Thanh Niên",
+            "namxb": "2019",
+            "sotrang": "186",
+            "kichthuoc": "13 x 20.5 cm",
+            "loaibia": "Bìa mềm",
+            "theloai": "Kỹ năng sống"
+        }
+    },
+    {
+        "masp": "Stt5",
+        "name": "Đừng Để Tiền Ngủ Yên",
+        "company": "NXB Trẻ",
+        "img": "img/products/Sach/Dungdetiennguyen.png",
+        "price": "25000",
         "star": 4,
-        "rateCount": 80,
+        "rateCount": 12,
         "promo": {
-            "name": "",
-            "value": ""
+            "name": "giamgia",
+            "value": "10000"
         },
         "info": {
-            "tacgia": "Rosie Nguyễn",
-            "nxb": "NXB Hội Nhà Văn",
-            "namxb": "2018",
-            "sotrang": "285",
-            "kichthuoc": "14 x 20.5 cm",
-            "loaibia": "Bìa mềm",
-            "theloai": "Kỹ năng sống"
-        }
-    },
-    {
-        "masp": "book4",
-        "name": "Cà Phê Cùng Tony",
-        "company": "NXB Trẻ",
-        "img": "img/products/Sach/ca-phe-cung-tony.jpg",
-        "price": "90000",
-        "amount": 70,
-        "star": 5,
-        "rateCount": 120,
-        "promo": {
-            "name": "",
-            "value": ""
-        },
-        "info": {
-            "tacgia": "Tony Buổi Sáng",
+            "tacgia": "Nhiều tác giả",
             "nxb": "NXB Trẻ",
-            "namxb": "2017",
-            "sotrang": "268",
-            "kichthuoc": "13 x 20 cm",
+            "namxb": "2020",
+            "sotrang": "168",
+            "kichthuoc": "13 x 20.5 cm",
             "loaibia": "Bìa mềm",
-            "theloai": "Kỹ năng sống"
+            "theloai": "Kinh tế"
         }
     },
     {
-        "masp": "book5",
-        "name": "Tôi Thấy Hoa Vàng Trên Cỏ Xanh",
-        "company": "NXB Trẻ",
-        "img": "img/products/Sach/toi-thay-hoa-vang-tren-co-xanh.jpg",
-        "price": "125000",
-        "amount": 60,
-        "star": 5,
-        "rateCount": 200,
+        "masp": "Stt6",
+        "name": "Hãy Nhớ Tên Anh Ấy",
+        "company": "NXB Văn Học",
+        "img": "img/products/Sach/Haynhotenanhay.jpg",
+        "price": "31000",
+        "star": 4,
+        "rateCount": 10,
         "promo": {
-            "name": "",
-            "value": ""
+            "name": "giareonline",
+            "value": "27000"
+        },
+        "info": {
+            "tacgia": "Nhiều tác giả",
+            "nxb": "NXB Văn Học",
+            "namxb": "2019",
+            "sotrang": "256",
+            "kichthuoc": "13 x 20.5 cm",
+            "loaibia": "Bìa mềm",
+            "theloai": "Tiểu thuyết"
+        }
+    },
+    {
+        "masp": "Stt7",
+        "name": "Hôm Nay Tôi Thất Tình",
+        "company": "NXB Văn Học",
+        "img": "img/products/Sach/Homnaytoithattinh.jpg",
+        "price": "46000",
+        "star": 4,
+        "rateCount": 15,
+        "promo": {
+            "name": "moiramat",
+            "value": "0"
+        },
+        "info": {
+            "tacgia": "Hạ Vũ",
+            "nxb": "NXB Văn Học",
+            "namxb": "2020",
+            "sotrang": "248",
+            "kichthuoc": "13 x 20.5 cm",
+            "loaibia": "Bìa mềm",
+            "theloai": "Tiểu thuyết"
+        }
+    },
+    {
+        "masp": "Stt8",
+        "name": "Làm Bạn Với Bầu Trời",
+        "company": "NXB Trẻ",
+        "img": "img/products/Sach/Lambanvoibautroi.jpg",
+        "price": "62000",
+        "star": 5,
+        "rateCount": 25,
+        "promo": {
+            "name": "giamgia",
+            "value": "15000"
         },
         "info": {
             "tacgia": "Nguyễn Nhật Ánh",
-            "nxb": "NXB Trẻ", 
-            "namxb": "2018",
-            "sotrang": "378",
+            "nxb": "NXB Trẻ",
+            "namxb": "2019",
+            "sotrang": "208",
             "kichthuoc": "13 x 20 cm",
             "loaibia": "Bìa mềm",
             "theloai": "Văn học Việt Nam"
+        }
+    },
+    {
+        "masp": "Stt9",
+        "name": "Người Đón Tàu",
+        "company": "NXB Văn Học",
+        "img": "img/products/Sach/Nguoidontau.jpg",
+        "price": "59000",
+        "star": 4,
+        "rateCount": 18,
+        "promo": {
+            "name": "moiramat",
+            "value": "0"
+        },
+        "info": {
+            "tacgia": "Nhiều tác giả",
+            "nxb": "NXB Văn Học",
+            "namxb": "2020",
+            "sotrang": "186",
+            "kichthuoc": "13 x 20.5 cm",
+            "loaibia": "Bìa mềm",
+            "theloai": "Tiểu thuyết"
         }
     }
 ];
