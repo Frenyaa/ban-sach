158d117da125323bf18c40fa81b05cf788914159
Bùi Đoàn Hải Yến <buidoanhaiyen@gmail.com>
2025-04-12 16:07:50 +0700

    Thêm dữ liệu mẫu cho đơn hàng và khách hàng

---
+++ data/orders.js
@@ -0,0 +1,141 @@
+var list_orders = [
+    {
+        "ma_don": "DH001",
+        "khach_hang": {
+            "ho_ten": "Nguyễn Văn An",
+            "email": "nguyenvanan@email.com",
+            "sdt": "0901234567",
+            "dia_chi": "123 Nguyễn Huệ, Q.1, TP.HCM"
+        },
+        "san_pham": [
+            {
+                "masp": "Stt1",
+                "so_luong": 2,
+                "don_gia": 49000
+            },
+            {
+                "masp": "Stt3",
+                "so_luong": 1,
+                "don_gia": 79000
+            }
+        ],
+        "tong_tien": 177000,
+        "ngay_dat": "2024-04-10",
+        "trang_thai": "Đã giao hàng"
+    },
+    {
+        "ma_don": "DH002",
+        "khach_hang": {
+            "ho_ten": "Trần Thị Bình",
+            "email": "tranbinh@email.com",
+            "sdt": "0912345678",
+            "dia_chi": "456 Lê Lợi, Q.5, TP.HCM"
+        },
+        "san_pham": [
+            {
+                "masp": "Stt2",
+                "so_luong": 1,
+                "don_gia": 69000
+            }
+        ],
+        "tong_tien": 69000,
+        "ngay_dat": "2024-04-11",
+        "trang_thai": "Đang xử lý"
+    },
+    {
+        "ma_don": "DH003",
+        "khach_hang": {
+            "ho_ten": "Lê Hoàng Cường",
+            "email": "lecuong@email.com",
+            "sdt": "0923456789",
+            "dia_chi": "789 Võ Văn Tần, Q.3, TP.HCM"
+        },
+        "san_pham": [
+            {
+                "masp": "Stt4",
+                "so_luong": 3,
+                "don_gia": 47000
+            },
+            {
+                "masp": "Stt5",
+                "so_luong": 2,
+                "don_gia": 25000
+            }
+        ],
+        "tong_tien": 191000,
+        "ngay_dat": "2024-04-11",
+        "trang_thai": "Đang giao hàng"
+    },
+    {
+        "ma_don": "DH004",
+        "khach_hang": {
+            "ho_ten": "Phạm Minh Đức",
+            "email": "pmduc@email.com",
+            "sdt": "0934567890",
+            "dia_chi": "321 Nguyễn Đình Chiểu, Q.3, TP.HCM"
+        },
+        "san_pham": [
+            {
+                "masp": "Stt8",
+                "so_luong": 1,
+                "don_gia": 62000
+            }
+        ],
+        "tong_tien": 62000,
+        "ngay_dat": "2024-04-12",
+        "trang_thai": "Đã giao hàng"
+    }
+];
+
+var list_customers = [
+    {
+        "username": "nguyenvanan",
+        "ho": "Nguyễn Văn",
+        "ten": "An",
+        "email": "nguyenvanan@email.com",
+        "sdt": "0901234567",
+        "dia_chi": "123 Nguyễn Huệ, Q.1, TP.HCM",
+        "mat_khau": "123456",
+        "trang_thai": "Hoạt động"
+    },
+    {
+        "username": "tranbinh",
+        "ho": "Trần Thị",
+        "ten": "Bình",
+        "email": "tranbinh@email.com",
+        "sdt": "0912345678",
+        "dia_chi": "456 Lê Lợi, Q.5, TP.HCM",
+        "mat_khau": "123456",
+        "trang_thai": "Hoạt động"
+    },
+    {
+        "username": "lecuong",
+        "ho": "Lê Hoàng",
+        "ten": "Cường",
+        "email": "lecuong@email.com",
+        "sdt": "0923456789",
+        "dia_chi": "789 Võ Văn Tần, Q.3, TP.HCM",
+        "mat_khau": "123456",
+        "trang_thai": "Hoạt động"
+    },
+    {
+        "username": "pmduc",
+        "ho": "Phạm Minh",
+        "ten": "Đức",
+        "email": "pmduc@email.com",
+        "sdt": "0934567890",
+        "dia_chi": "321 Nguyễn Đình Chiểu, Q.3, TP.HCM",
+        "mat_khau": "123456",
+        "trang_thai": "Hoạt động"
+    },
+    {
+        "username": "thuhuong",
+        "ho": "Nguyễn Thu",
+        "ten": "Hương",
+        "email": "thuhuong@email.com",
+        "sdt": "0945678901",
+        "dia_chi": "147 Cách Mạng Tháng 8, Q.3, TP.HCM",
+        "mat_khau": "123456",
+        "trang_thai": "Hoạt động"
+    }
+]; 
